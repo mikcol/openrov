1f3e0a1729afe71b6e825889d02ab643363c7073
dubmelz0r <nhtheswede@gmail.com>
2014-07-02 23:16:03 +0200

    added test_2 output files

--- src/laserlines/src/lasertest_simulated.cpp
+++ src/laserlines/src/lasertest_simulated.cpp
@@ -209,7 +209,7 @@ int main(int argc, char **argv)
 		if(!img.data){ return -1;}
 
 		cvtColor(img,img,CV_BGR2HSV);
-		inRange(img,Scalar(40,200,10),Scalar(70,255,100),greenImg);
+		inRange(img,Scalar(40,200,10),Scalar(70,255,255),greenImg);
 
 		threshold(greenImg,bwImg,1,255,THRESH_BINARY);
 		// Convert to grayscale
